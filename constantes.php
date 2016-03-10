2dfb4c1c1d5f4c4d85c4e834bb35b81c3fab0ba2
Trenzalord <bsvalentin.pivet@outlook.fr>
2016-03-10 09:10:42 -0500

    Modification du stockage des constantes

---
+++ constantes.php
@@ -0,0 +1,6 @@
+<?php
+
+$documentRoot = "/GitEndTheStock/EndTheStock";
+
+?>
+
--- session.php
+++ session.php
@@ -5,6 +5,7 @@ session_start();
 include 'utilisateur/User.php';
 include 'admin/categorie/Categorie.php';
 include 'admin/produit/Produit.php';
+include 'utilisateur/panier/LigneCommande.php';
 
 if(isset($_SESSION['user'])){
     $isUserConnected = true;
@@ -18,4 +19,3 @@ if(isset($_SESSION['user'])){
     $isUserConnected = false;
 }
 
-$documentRoot = "";
