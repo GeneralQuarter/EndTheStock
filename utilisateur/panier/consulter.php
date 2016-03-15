fb8b0570124e9b6737bd3231b974a29ff54364ae
Trenzalord <bsvalentin.pivet@outlook.fr>
2016-03-15 11:22:35 -0400

    Vérification que l'utilise a renseigné une adresse pour pouvoir commander

--- utilisateur/panier/consulter.php
+++ utilisateur/panier/consulter.php
@@ -40,11 +40,57 @@ if($isCommande){
         </table>
         <h3 class="pull-right" >Prix total avec taxe : <?php echo $commande->getPrixAvecTaxeString() ?> $ CAD</h3>
     </div>
-    <div class="row"><a class="btn btn-success pull-right" href="enregistrerCommande.php" role="button">Passer la commande ...</a></div>
+    <?php 
+    if($user->getAdresse_id()!== null) {?>
+        <div class="row"><a class="btn btn-success pull-right" href="enregistrerCommande.php" role="button">Passer la commande</a></div>
+    <?php }else{ ?>
+        <div class="row">
+            <a class="btn btn-success pull-right" href="#" disabled="disabled" role="button">Passer la commande</a>
+        </div>
+        
+        <div class="row" style="margin-top: 30px;">
+            <button role="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#editAdresse" data-id=""><span class="glyphicon glyphicon-plus"></span> Ajouter une adresse</button>'
+            <p class="pull-right" style="margin: 10px;">Ajoutez une adresse pour pouvoir commander</p>
+        </div>
+        
+    <?php } ?>
     <div class="row"><a class="btn btn-danger pull-left" href="viderPanier.php" role="button">Vider le panier</a></div>
     
 </div>
             
 <?php } ?>
+
+<!-- EDIT MODAL -->
+<div class="modal fade" id="editAdresse" tabindex="-1" role="dialog" aria-labelledby="Editer Adresse">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title">Renseigner une adresse</h3>
+            </div>
+            <form action="../adresse/editer.php" method="POST">
+            <div class="modal-body">
+                <input type="hidden" id="inputId" name="idClient" value="<?php $user->getId()?>" />
+                <p>Numéro civique</p>
+                <input class="form-control" type="text" maxlength="6" id="inputNumCiv" name="numero_civique" value="" placeholder="ex : 17" required/><BR>
+                <p>Rue</p>
+                <input class="form-control" type="text" maxlength="100" id="inputRue" name="rue" value="" placeholder="ex : Avenue Charles de Gaulle" required/><BR>
+                <p>Département</p>
+                <input class="form-control" type="text" maxlength="100" id="inputDepartement" name="departement" value="" placeholder="ex : 17000" required/><BR>
+                <p>Ville</p>
+                <input class="form-control" type="text" maxlength="100" id="inputVille" name="ville" value="" placeholder="ex : La Rochelle" required/><BR>
+                <p>Region</p>
+                <input class="form-control" type="text" maxlength="100" id="inputRegion" name="region" value="" placeholder="ex : Poitou-Charante" required/><BR>
+                <p>Pays</p>
+                <input class="form-control" type="text" maxlength="100" id="inputPays" name="pays" value="" placeholder="ex : France" required/><BR>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                <input type="submit" class="btn btn-success"  id="comfirmButton" value="Comfirmer">
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php include '../../footer.php'; ?>
